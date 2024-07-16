f8dd39ebb1a2557e76aba55947fff60f691d88af
Kolad2 <koladik@gmail.com>
2024-07-16 04:33:44 +0300

    Commit home 1

--- SE/distrebutions.py
+++ SE/distrebutions.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 from scipy.special import erf
+from scipy.special import gammainc
+from scipy.special import gamma
 
 def pareto(x, a, xmin):
     return ((a-1)/xmin)*((x/xmin)**(-a))
@@ -68,9 +70,33 @@ class lognorm:
     def __init__(self, sigma, scale):
         self.sigma = sigma
         self.scale = scale
-        
+
     def pdf(self, x):
         return pdf_lognorm(x, self.sigma, self.scale)
 
     def cdf(self, x):
         return cdf_lognorm(x, self.sigma, self.scale)
+
+def сdf_gengamma(x, a, b, lam):
+    return gammainc(a, np.power(x/lam, b))
+
+def pdf_gengamma(x,a,b,lam):
+    return b/lam/gamma(a)*np.power(x/lam, a*b-1)*np.exp(-np.power(x/lam,b))
+
+class gengamma:
+    def __init__(self, a, b, lam):
+        self.a = a
+        self.b = b
+        self.lam = lam
+
+    def pdf(self, x):
+        return pdf_gengamma(x, self.a, self.b, self.lam)
+
+    def cdf(self, x):
+        return cdf_gengamma(x, self.a, self.b, self.lam)
+
+    def sumlogpdf(self, x, MlnX):
+        W1 = -math.ln(gamma(self.a)) + math.ln(self.b) - math.ln(lam)
+        W2 = (self.a*self.b-1)*(MlnX - math.ln(lam))
+        W3 = -np.mean(np.power(x/lam, b))
+        return W1 + W2 + W3
